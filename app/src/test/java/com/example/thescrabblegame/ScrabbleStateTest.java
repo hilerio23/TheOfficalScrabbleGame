d651e87d48289b6f18da431cddd2858c17a80f72
hilerio23 <hilerio23@up.edu>
2021-04-06 20:53:42 -0700

    Added a unit testing class

---
+++ app/src/test/java/com/example/thescrabblegame/ScrabbleStateTest.java
@@ -0,0 +1,45 @@
+package com.example.thescrabblegame;
+
+import junit.framework.TestCase;
+
+public class ScrabbleStateTest extends TestCase {
+
+    public void testTestToString() {
+    }
+
+    public void testSetIdNum() {
+    }
+
+    public void testSetPlayer1Score() {
+    }
+
+    public void testSetPlayer2Score() {
+    }
+
+    public void testSetPlayer3Score() {
+    }
+
+    public void testSetPlayer4Score() {
+    }
+
+    public void testSetBoard() {
+    }
+
+    public void testGetIdNum() {
+    }
+
+    public void testGetPlayer1Score() {
+    }
+
+    public void testGetPlayer2Score() {
+    }
+
+    public void testGetPlayer3Score() {
+    }
+
+    public void testGetPlayer4Score() {
+    }
+
+    public void testGetBoard() {
+    }
+}
